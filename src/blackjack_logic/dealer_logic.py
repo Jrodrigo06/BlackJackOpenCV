9241019c45fe0cd82ff4104eaf5ea04edf4ec522
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-03-26 19:42:46 -0400

    Added exception adn logging and started dealer logic

---
+++ src/blackjack_logic/dealer_logic.py
@@ -0,0 +1,6 @@
+from multiset import *
+
+
+#Initializes the deck of cards, as a multiset for 52 cards
+deck = Multiset({'Ace': 4, 'King' : 4, 'Queen' : 4, 'Jack' : 4, '10' : 4, '9' : 4, '8' : 4, '7' : 4, '6' : 4, '5' : 4, '4' : 4, '3' : 4, '2' : 4})
+
